3789cf011ef8b91c24d4e029b046790dceef351d
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-16 22:27:37 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,29 @@
+function desen(n){
+    var i=n;
+    while(i<=n){
+        console.log(i);
+        i--;
+    }
+}
+
+desen(10);
+
+
+function even(n) {
+    var i = 0;
+    do {
+        console.log(i);
+        i = i + 2 ;
+    }while (i > 0);
+}
+
+even(10);
+function even(n) {
+    var i = 1;
+    do {
+        console.log(i);
+        i = i + 2 ;
+    }while (i > 0);
+}
+
+odd(10);
